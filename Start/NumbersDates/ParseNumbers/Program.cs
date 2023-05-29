2b189b1666e5e76b230a94fef2141d90a052794a
KristianSundhaugen <kristiansundhaugen@gmail.com>
2023-05-29 15:43:56 +0200

    NumbersDates ParseNumbers .NET exercise

--- Start/NumbersDates/ParseNumbers/Program.cs
+++ Start/NumbersDates/ParseNumbers/Program.cs
@@ -10,8 +10,28 @@ bool result;
 // TODO: The Parse method attempts to parse a string to a number and
 // throws an exception if the parse is unsuccessful
 foreach (string str in NumStrs) {
-
+    try
+    {
+        testfloat = float.Parse(str);
+        System.Console.WriteLine($"Parsed number is {testfloat}");
+        testint = int.Parse(str);
+        System.Console.WriteLine($"Parsed int is {testint}");
+    }
+    catch (System.FormatException e)
+    {
+        System.Console.WriteLine($"Could not parse '{str}' : {e.Message}");    
+    }
 }
 
 // TODO: The TryParse method returns 'true' if the parse is successful
+result = int.TryParse(NumStrs[0], out testint);
+System.Console.WriteLine($"{result} --  '{NumStrs[0]}' : {testint}");
 
+result = float.TryParse(NumStrs[1], out testfloat);
+System.Console.WriteLine($"{result} --  '{NumStrs[1]}' : {testfloat}");
+
+result = int.TryParse(NumStrs[2], out testint);
+System.Console.WriteLine($"{result} --  '{NumStrs[2]}' : {testint}");
+
+result = float.TryParse(NumStrs[3], out testfloat);
+System.Console.WriteLine($"{result} --  '{NumStrs[3]}' : {testfloat}");
